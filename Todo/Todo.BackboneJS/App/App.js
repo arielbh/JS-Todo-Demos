939c0b33abf77a523011f26aad50087d657c6d8f
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-06-05 12:44:38 +0300

    Backbone Step-11 Show a hello world, view. Show binding a collection

--- Todo/Todo.BackboneJS/App/App.js
+++ Todo/Todo.BackboneJS/App/App.js
@@ -1,2 +1,6 @@
 ﻿$(function () {
+    var appView = new AppView();
+    appView.addItem(new Todo({ title: "Throw" }));
+    appView.addItem(new Todo({ title: "Throw 2" }));
+    
 });
---
+++ Todo/Todo.BackboneJS/App/AppView.js
@@ -0,0 +1,34 @@
+﻿AppView = Backbone.View.extend({
+    el: $("#todoapp"),
+
+    initialize: function () {
+        _.bindAll(this, 'render', "addItem", "appendItem"); // fixes loss of context for 'this' within methods
+        this.source = new Todos();
+        this.source.bind("add", this.appendItem);
+
+        this.render(); // not all views are self-rendering. This one is.
+    },
+
+    render: function () {
+        
+
+        $(this.el).append("<ul></ul>");
+        if (this.source) {
+            _(this.source.models).each(function (item) { // in case collection is not empty
+                self.appendItem(item);
+            }, this);
+        }
+        return this;
+    },
+    
+    addItem: function (todo) {
+        this.source.add(todo);
+    },
+
+
+    appendItem: function (item) {
+        $('ul', this.el).append("<li>" + item.get('title') + "</li>");
+    }
+
+
+});
