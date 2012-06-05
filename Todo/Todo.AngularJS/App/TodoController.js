1f6dd260b0ee6663bdc8e52235bb0d5693902ac5
Ariel Ben Horesh <ariel_bh@yahoo.com>
2012-06-05 17:22:09 +0300

    Angular Step-17 Show how to add input checkbox binding and form + input for adding a new todo

--- Todo/Todo.AngularJS/App/TodoController.js
+++ Todo/Todo.AngularJS/App/TodoController.js
@@ -4,5 +4,12 @@ function TodoController($scope) {
         new Todo("task 1", false),
         new Todo("task 2", false)
     ];
+
+    $scope.newTodo = "";
+
+    $scope.addTodo = function () {
+        $scope.todos.push(new Todo($scope.newTodo, false));
+
+    };
     
 }
